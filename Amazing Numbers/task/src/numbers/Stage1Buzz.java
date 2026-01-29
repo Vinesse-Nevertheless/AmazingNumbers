4089a6ca43fc8d447ef04aabea6d9fef6828a8ed
Vanessa Reese <88684757+Vinesse-Nevertheless@users.noreply.github.com>
2026-01-29 01:53:55 -0500

    Stage 1 Buzz Numbers
    stage: 1
        title: "Buzz Numbers"
        commit_header: "feat: parity check and buzz number logic"
        description: "Initial setup for natural number input. Implemented modular arithmetic to identify Even, Odd, and Buzz numbers (divisible by 7 or ending in 7)."

---
+++ Amazing Numbers/task/src/numbers/Stage1Buzz.java
@@ -0,0 +1,74 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class Stage1Buzz {
+    public static void main(String[] args) {
+        Stage1Buzz init = new Stage1Buzz();
+        init.requestNumber();
+    }
+
+
+    void requestNumber() {
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Enter a natural number: ");
+            int num = in.nextInt();
+
+            if (!isNaturalNum(num)) {
+                return;
+            }
+
+            checkOddOrEven(num);
+            checkIfBuzzNum(num);
+
+        } catch (Exception e) {
+            System.out.println("This is not a whole number.");
+        }
+    }
+
+    boolean isNaturalNum(int num) {
+        if (num < 1) {
+            System.out.println("This number is not natural!");
+            return false;
+        }
+        return true;
+    }
+
+    void checkOddOrEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println("This number is Even.");
+        } else {
+            System.out.println("This number is Odd.");
+        }
+    }
+
+    void checkIfBuzzNum(int num) {
+        StringBuilder reason = new StringBuilder();
+        String numStr = String.valueOf(num);
+
+        if (num % 7 == 0) {
+            reason.append(num).append(" is divisible by 7.");
+        }
+        if (numStr.charAt(numStr.length() - 1) == '7') {
+            if (reason.isEmpty()) {
+                reason.append(num).append(" ends with 7.");
+            } else {
+                reason.deleteCharAt(reason.length() - 1);
+                reason.append(" and ends with 7.");
+            }
+        }
+
+
+        if (reason.isEmpty()) {
+            System.out.println("It is not a Buzz number.");
+            System.out.println("Explanation: ");
+            System.out.println(num + " is neither divisible by 7 nor does it end with 7.");
+        } else {
+            System.out.println("It is a Buzz number.");
+            System.out.println("Explanation: ");
+            System.out.println(reason);
+        }
+
+    }
+}
